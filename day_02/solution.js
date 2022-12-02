da690d51f2f5d9b8cec34c48b8a08584874566bf
adamczarnecki <adam@czarnecki.cz>
2022-12-02 12:56:20 +0100

    day 02 more like typescript

--- day_02/solution.js
+++ day_02/solution.js
@@ -2,13 +2,6 @@
 // usefull consts
 const singleLineSplit = /\r?\n/;
 const doubleLineSplit = /\r?\n\r?\n/;
-function solution1(input) {
-    return input.reduce((a, x) => { return a + playOneGame(...x.split(' ')); }, 0);
-}
-function solution2(input) {
-    return input.reduce((a, x) => { return a + playOtherGame(...x.split(' ')); }, 0);
-}
-// 
 // oponent:     A Rock B Paper C Scissors
 // Me:          X Rock Y Paper Z Scissors
 // Score:       1 Rock 2 Peper 3 Scissors for my choses
@@ -19,69 +12,78 @@ var GameScore;
     GameScore[GameScore["draw"] = 3] = "draw";
     GameScore[GameScore["win"] = 6] = "win";
 })(GameScore || (GameScore = {}));
-var FigurePoint;
-(function (FigurePoint) {
-    FigurePoint[FigurePoint["rock"] = 1] = "rock";
-    FigurePoint[FigurePoint["paper"] = 2] = "paper";
-    FigurePoint[FigurePoint["scissors"] = 3] = "scissors";
-})(FigurePoint || (FigurePoint = {}));
-function playOneGame(oponent, me) {
-    const MyPoints = {
-        'X': FigurePoint.rock,
-        'Y': FigurePoint.paper,
-        'Z': FigurePoint.scissors
-    };
+var Figure;
+(function (Figure) {
+    Figure[Figure["rock"] = 1] = "rock";
+    Figure[Figure["paper"] = 2] = "paper";
+    Figure[Figure["scissors"] = 3] = "scissors";
+})(Figure || (Figure = {}));
+const figureDescriptons = {
+    A: Figure.rock,
+    B: Figure.paper,
+    C: Figure.scissors,
+    X: Figure.rock,
+    Y: Figure.paper,
+    Z: Figure.scissors
+};
+function solution1(input) {
     const gameTree = {
-        'A': {
-            'X': GameScore.draw,
-            'Y': GameScore.win,
-            'Z': GameScore.lost
+        [Figure.rock]: {
+            [Figure.rock]: GameScore.draw,
+            [Figure.paper]: GameScore.win,
+            [Figure.scissors]: GameScore.lost
         },
-        'B': {
-            'X': GameScore.lost,
-            'Y': GameScore.draw,
-            'Z': GameScore.win
+        [Figure.paper]: {
+            [Figure.rock]: GameScore.lost,
+            [Figure.paper]: GameScore.draw,
+            [Figure.scissors]: GameScore.win
         },
-        'C': {
-            'X': GameScore.win,
-            'Y': GameScore.lost,
-            'Z': GameScore.draw
+        [Figure.scissors]: {
+            [Figure.rock]: GameScore.win,
+            [Figure.paper]: GameScore.lost,
+            [Figure.scissors]: GameScore.draw
         }
     };
-    return gameTree[oponent][me] + MyPoints[me];
+    return input.reduce((a, x) => {
+        const [oponent, me] = [figureDescriptons[x.split(' ')[0]], figureDescriptons[x.split(' ')[1]]];
+        return a + gameTree[oponent][me] + me;
+    }, 0);
 }
 // oponent:     A Rock B Paper C Scissors
 // I shuold:    X lose Y draw  Z win
 // Score:       1 Rock 2 Peper 3 Scissors for my choses
 // and outcome: 0 lost 3 draw  6 win
-function playOtherGame(oponent, me) {
+function solution2(input) {
     const GameResults = {
         'X': GameScore.lost,
         'Y': GameScore.draw,
         'Z': GameScore.win
     };
     const myFigure = {
-        'A': {
-            'X': FigurePoint.scissors,
-            'Y': FigurePoint.rock,
-            'Z': FigurePoint.paper
+        [Figure.rock]: {
+            [GameScore.lost]: Figure.scissors,
+            [GameScore.draw]: Figure.rock,
+            [GameScore.win]: Figure.paper
         },
-        'B': {
-            'X': FigurePoint.rock,
-            'Y': FigurePoint.paper,
-            'Z': FigurePoint.scissors
+        [Figure.paper]: {
+            [GameScore.lost]: Figure.rock,
+            [GameScore.draw]: Figure.paper,
+            [GameScore.win]: Figure.scissors
         },
-        'C': {
-            'X': FigurePoint.paper,
-            'Y': FigurePoint.scissors,
-            'Z': FigurePoint.rock
+        [Figure.scissors]: {
+            [GameScore.lost]: Figure.paper,
+            [GameScore.draw]: Figure.scissors,
+            [GameScore.win]: Figure.rock
         }
     };
-    return GameResults[me] + myFigure[oponent][me];
+    return input.reduce((a, x) => {
+        const [oponent, result] = [figureDescriptons[x.split(' ')[0]], GameResults[x.split(' ')[1]]];
+        return a + result + myFigure[oponent][result];
+    }, 0);
 }
 // boilerprint
 function job(input) {
-    console.log(input);
+    // console.log(input)
     const span1 = document.querySelector('#answer1');
     const answer1 = solution1(input);
     span1.innerText = typeof answer1 == 'number' ? answer1.toString() : answer1;
--- day_02/solution.ts
+++ day_02/solution.ts
@@ -2,14 +2,6 @@
 const singleLineSplit = /\r?\n/
 const doubleLineSplit = /\r?\n\r?\n/
 
-function solution1(input:string[]): (string|number){
-  return input.reduce((a, x) => {return a + playOneGame(...x.split(' ')) },0)
-}
-
-function solution2(input:string[]):( string|number){
-  return input.reduce((a, x) => {return a + playOtherGame(...x.split(' ')) },0)
-}
-// 
 // oponent:     A Rock B Paper C Scissors
 // Me:          X Rock Y Paper Z Scissors
 // Score:       1 Rock 2 Peper 3 Scissors for my choses
@@ -21,77 +13,85 @@ enum GameScore {
   win = 6
 }
 
-enum FigurePoint {
+enum Figure {
   rock = 1,
   paper = 2,
   scissors = 3
 }
 
-function playOneGame(oponent:string, me:string): number {
-  const MyPoints = {
-    'X' : FigurePoint.rock,
-    'Y' : FigurePoint.paper,
-    'Z' : FigurePoint.scissors
-  }
-  const gameTree = {
-    'A': {
-      'X': GameScore.draw,
-      'Y': GameScore.win,
-      'Z': GameScore.lost
+const figureDescriptons: Record<string, Figure> = {
+  A: Figure.rock,
+  B: Figure.paper,
+  C: Figure.scissors,
+  X: Figure.rock,
+  Y: Figure.paper,
+  Z: Figure.scissors
+}
+
+function solution1(input:string[]): (string|number){
+  const gameTree: Record<Figure, Record<Figure, GameScore>> = {
+    [Figure.rock]: {
+      [Figure.rock]: GameScore.draw,
+      [Figure.paper]: GameScore.win,
+      [Figure.scissors]: GameScore.lost
     },
-    'B': {
-      'X': GameScore.lost,
-      'Y': GameScore.draw,
-      'Z': GameScore.win
+    [Figure.paper]: {
+      [Figure.rock]: GameScore.lost,
+      [Figure.paper]: GameScore.draw,
+      [Figure.scissors]: GameScore.win
     },
-    'C': {
-      'X': GameScore.win,
-      'Y': GameScore.lost,
-      'Z': GameScore.draw
+    [Figure.scissors]: {
+      [Figure.rock]: GameScore.win,
+      [Figure.paper]: GameScore.lost,
+      [Figure.scissors]: GameScore.draw
     }
   }
 
-  return gameTree[oponent][me] + MyPoints[me]
+  return input.reduce((a, x) => {
+    const [oponent, me] = [figureDescriptons[x.split(' ')[0]], figureDescriptons[x.split(' ')[1]]];
+    return a + gameTree[oponent][me] + me
+  },0)
 }
-
 
 // oponent:     A Rock B Paper C Scissors
 // I shuold:    X lose Y draw  Z win
 // Score:       1 Rock 2 Peper 3 Scissors for my choses
 // and outcome: 0 lost 3 draw  6 win
 
-function playOtherGame(oponent: string, me: string) {
-  const GameResults = {
+function solution2(input:string[]):( string|number){
+  const GameResults:Record<string, GameScore>  = {
     'X': GameScore.lost,
     'Y': GameScore.draw,
     'Z': GameScore.win
   }
 
-  const myFigure ={
-    'A': {
-      'X': FigurePoint.scissors,
-      'Y': FigurePoint.rock,
-      'Z': FigurePoint.paper
+  const myFigure:Record<Figure, Record<GameScore, Figure>>  = {
+    [Figure.rock]: {
+      [GameScore.lost]: Figure.scissors,
+      [GameScore.draw]: Figure.rock,
+      [GameScore.win]: Figure.paper
     },
-    'B': {
-      'X': FigurePoint.rock,
-      'Y': FigurePoint.paper,
-      'Z': FigurePoint.scissors
+    [Figure.paper]: {
+      [GameScore.lost]: Figure.rock,
+      [GameScore.draw]: Figure.paper,
+      [GameScore.win]: Figure.scissors
     },
-    'C': {
-      'X': FigurePoint.paper,
-      'Y': FigurePoint.scissors,
-      'Z': FigurePoint.rock
+    [Figure.scissors]: {
+      [GameScore.lost]: Figure.paper,
+      [GameScore.draw]: Figure.scissors,
+      [GameScore.win]: Figure.rock
     }
   }
-
-  return GameResults[me] + myFigure[oponent][me]
+  return input.reduce((a, x) => {
+    const [oponent, result] = [figureDescriptons[x.split(' ')[0]], GameResults[x.split(' ')[1]]];
+    return a + result + myFigure[oponent][result]
+  },0)
 }
 
 
 // boilerprint
 function job(input:string[]): void {
-  console.log(input)
+  // console.log(input)
   const span1 = document.querySelector('#answer1') as HTMLElement
   const answer1: (string|number) = solution1(input)
   span1.innerText = typeof answer1 == 'number' ? answer1.toString() : answer1
